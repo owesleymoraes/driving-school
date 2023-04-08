d41fbcea52e24c5380e22c4e48d7f8b042b7c48d
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-08 16:58:10 -0300

    feat: adding media query in Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,15 +1,30 @@
 import styled from "styled-components";
 import img from "../../assets/car.jpg";
+import { screenBreakPoint, BreakPointSize } from "../../BreakPointSize";
 
 export const HeaderContainer = styled.div`
   color: #fff;
+  padding: 100px 0;
   background: url(${img}), rgba(0, 0, 0, 0.4);
   background-size: cover;
   background-position: center;
   background-blend-mode: overlay;
 `;
 
-export const WrapperHeader = styled.div``;
+export const WrapperHeader = styled.div`
+  max-width: 100%;
+  padding: 0 8px;
+
+  ${screenBreakPoint(BreakPointSize.sm)} {
+    padding: 0 16px;
+  }
+
+  ${screenBreakPoint(BreakPointSize.xl)} {
+    width: 1140px;
+    padding: 0;
+    margin: 0 auto;
+  }
+`;
 
 export const TitleHeader = styled.h1`
   font-size: 32px;
