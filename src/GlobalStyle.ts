3dc3fa8199ff8f2d33202252df437ec16cde9c2d
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-09 21:06:30 -0300

    feat: adding type of title by tag h1, h2, h3, h4, h5, h6 in GlobalStyle

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,5 +1,6 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { screenBreakPoint, BreakPointSize } from "./BreakPointSize";
 
 const GlobalStyle = createGlobalStyle`
         @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap");
@@ -17,6 +18,62 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         margin: 0;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        line-height: 1.3;
+        font-weight: 700;
+        letter-spacing: 2px;
+    }
+
+    h1 {
+        font-size: 2.5rem;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 3.75rem;
+        }
+    }
+    h2 {
+        font-size: 2rem;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 3.125rem;
+        }
+    }
+
+    h3 {
+        font-size: 1.9rem;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 2.5rem;
+        }
+    }
+
+    h4 {
+        font-size: 1.3rem;
+        font-weight: 600;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 2.125rem;
+        }
+    }
+
+    h5 {
+        font-size: 1.2rem;
+        font-weight: 300;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 1.5rem;
+        }
+    }
+
+    h6 {
+        font-size: 1.1rem;
+        font-weight: 600;
+
+        ${screenBreakPoint(BreakPointSize.lg)} {
+            font-size: 1.25rem;
+        }
     }
 `;
 export default GlobalStyle;
