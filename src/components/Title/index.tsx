e1bd440475abbbd0dc7042fd7f38820db0db3cc9
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-12 23:50:50 -0300

    feat: implemeted in Title component the BottonBar compoenent

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import * as Styled from "./style";
+import { BottomBar } from "../BottomBar";
 
 interface TitleProps {
   description: React.ReactElement | string;
 }
 
 export const Title: React.FC<TitleProps> = ({ description }) => {
-  return <Styled.Title>{description}</Styled.Title>;
+  return (
+    <>
+      <Styled.Title>{description}</Styled.Title>
+      <BottomBar colorBar={"orange"} />
+    </>
+  );
 };
--- src/components/Title/style.ts
+++ src/components/Title/style.ts
@@ -4,24 +4,12 @@ const yellowCustom = "#ffc107";
 
 export const Title = styled.h1`
   position: relative;
-  width: 100%;
   letter-spacing: 2px;
   font-weight: 700;
-  margin-bottom: 24px;
-  padding-bottom: 24px;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  margin-bottom: 4px;
+  padding-bottom: 4px;
 
   strong {
     color: ${yellowCustom};
-  }
-
-  &::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: -4px;
-    background-color: ${yellowCustom};
-    height: 8px;
-    width: 70px;
   }
 `;
