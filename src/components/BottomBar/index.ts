594f5647e3ac9801d1b371009d9fb073386bc934
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-04-13 23:07:16 -0300

    feat: clean code in two files

--- src/components/BottomBar/index.ts
+++ src/components/BottomBar/index.ts
@@ -1,15 +1,5 @@
 import styled from "styled-components";
 
-const resolveColor = (type: string) => {
-  if (type === "orange") return "#ffc107";
-
-  if (type === "black") return "#000000";
-
-  if (type === "white") return "#ffffff";
-
-  if (type === "red") return " #ff0000";
-};
-
 export const BottomBar = styled.div<{ colorBar: string }>`
   position: relative;
   width: 100%;
@@ -22,7 +12,6 @@ export const BottomBar = styled.div<{ colorBar: string }>`
     left: 0;
     bottom: -4px;
     background-color: #ffc107;
-    /* background-color:( ${(props) => resolveColor(props.colorBar)}); */
     height: 8px;
     width: 70px;
   }
