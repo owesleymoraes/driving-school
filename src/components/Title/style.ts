781ed5ae04f2c042954b1d936d46ff740b8b5d3c
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-28 15:53:52 -0300

    feat: implementation in size heading in description of the component

--- src/components/Title/style.ts
+++ src/components/Title/style.ts
@@ -2,7 +2,55 @@ import styled from "styled-components";
 
 const yellowCustom = "#ffc107";
 
-export const Title = styled.h1`
+export const H1 = styled.h1`
+  position: relative;
+  letter-spacing: 2px;
+  font-weight: 700;
+  margin-bottom: 4px;
+  padding-bottom: 4px;
+
+  strong {
+    color: ${yellowCustom};
+  }
+`;
+
+export const H2 = styled.h2`
+  position: relative;
+  letter-spacing: 2px;
+  font-weight: 700;
+  margin-bottom: 4px;
+  padding-bottom: 4px;
+
+  strong {
+    color: ${yellowCustom};
+  }
+`;
+
+export const H3 = styled.h3`
+  position: relative;
+  letter-spacing: 2px;
+  font-weight: 700;
+  margin-bottom: 4px;
+  padding-bottom: 4px;
+
+  strong {
+    color: ${yellowCustom};
+  }
+`;
+
+export const H4 = styled.h4`
+  position: relative;
+  letter-spacing: 2px;
+  font-weight: 700;
+  margin-bottom: 4px;
+  padding-bottom: 4px;
+
+  strong {
+    color: ${yellowCustom};
+  }
+`;
+
+export const H5 = styled.h5`
   position: relative;
   letter-spacing: 2px;
   font-weight: 700;
