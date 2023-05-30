f071b93523e341b653917fb1490e02937c280333
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-05-29 22:42:43 -0300

    feat: implemeted section about in home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Grid } from "../../components/Grid";
+import { Title } from "../../components/Title";
 import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import trafficCar from "../../assets/traffic.mp4";
 import { Feature } from "../../components/Features";
 import { Sections } from "../../components/Sections";
 import { FaAccessibleIcon, FaCar, FaKey, FaMapMarkerAlt } from "react-icons/fa";
-import { Title } from "../../components/Title";
-import { BottomBar } from "../../components/BottomBar";
+
+import * as Styled from "./styles";
 
 export const HomePage: React.FC = () => {
   const itemList = ["item 1", "item 2", "item 3", "item 4", "item 5"];
@@ -43,9 +46,25 @@ export const HomePage: React.FC = () => {
       <Sections inverse>
         <Title tagOfTitle="h2" description={"Conheça nossos serviços"}></Title>
       </Sections>
-      <Sections>
-        <Title tagOfTitle="h2" description={"Moraes Auto Escola"}></Title>
-      </Sections>
+
+      <Styled.SectionAbout>
+        <Sections>
+          <Title tagOfTitle="h2" description={"Moraes Auto Escola"} />
+          <Styled.InformationText>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quod
+            similique aperiam amet quaerat error repudiandae dolorum. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Sequi quod
+            similique aperiam amet quaerat error repudiandae dolorum.
+          </Styled.InformationText>
+          <Button height={14} fontSize={12} onClick={() => {}}>
+            Saiba mais
+          </Button>
+        </Sections>
+        <Styled.ContainerVideo>
+          <video src={trafficCar} width="100%" autoPlay loop />
+        </Styled.ContainerVideo>
+      </Styled.SectionAbout>
+
       <Sections inverse>
         <Title tagOfTitle="h2" description={"Dúvidas recorrentes"}></Title>
       </Sections>
