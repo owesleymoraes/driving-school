dc88c95fc6b8c8b51a9110ef4a6e68a5eb928772
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-06-01 22:41:33 -0300

    feat: created card component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ContainerCard = styled.div`
+  display: flex;
+  max-width: 360px;
+  width: 320px;
+  flex-direction: column;
+  border-radius: 4px;
+  background-color: #ffff;
+`;
+
+export const Image = styled.img`
+  max-width: 320px;
+  width: 100%;
+  margin-top: 16px;
+`;
+
+export const TitleCard = styled.div`
+  margin-top: 8px;
+  white-space: normal;
+  width: 230px;
+`;
+
+export const DescriptionCard = styled.span`
+margin-top: 16px;
+margin-bottom: 24px;
+`;
+
+export const ButtonCard = styled.div``;
