3aebefb29a656cc20b46ff5dfe5ae259a095f788
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-06-13 22:34:11 -0300

    feat: adding atribute title to header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,28 +6,39 @@ import { Container } from "../Container";
 import * as Styled from "./styles";
 
 interface HeaderProps {
-  children: string[];
+  children?: string[];
+  titlePrincipal?: string;
+  titlePrimary?: string;
+  titleSecondary?: string;
 }
 
-export const Header: React.FC<HeaderProps> = ({ children }) => {
+export const Header: React.FC<HeaderProps> = ({
+  children,
+  titlePrincipal,
+  titlePrimary,
+  titleSecondary,
+}) => {
   return (
     <Styled.HeaderContainer>
       <Container>
         <Title
           description={
             <span>
-              Ganhe sua <strong>Liberdade</strong>
-              <br /> para ir e vir
+              {titlePrincipal} {titlePrimary && <strong>{titlePrimary}</strong>}
+              <br /> {titleSecondary}
             </span>
           }
         />
         <Styled.InfoHeaderContainer>
           <Styled.InfoHeaderContainer>
-            {children.map((item, key) => {
-              return (
-                <Styled.InfoHeaderList key={key}>{item}</Styled.InfoHeaderList>
-              );
-            })}
+            {children &&
+              children.map((item, key) => {
+                return (
+                  <Styled.InfoHeaderList key={key}>
+                    {item}
+                  </Styled.InfoHeaderList>
+                );
+              })}
           </Styled.InfoHeaderContainer>
         </Styled.InfoHeaderContainer>
         <Button
