ee9c80d01e43320adba8b477647bbabcd4ed7e13
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-06-15 22:11:03 -0300

    feat: created svg component

--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const ContainerAbout = styled.div``
+export const ContainerAbout = styled.div``;
+
+export const Image = styled.div`
+  svg {
+    color: #ffc107;
+    max-width: 380px;
+    height: auto;
+    width: 100%;
+  }
+`;
