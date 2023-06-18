4487074492dd0be505629e21866f18cea76462ec
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-06-18 16:16:42 -0300

    feat: finish about page

--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -2,6 +2,22 @@ import styled from "styled-components";
 
 export const ContainerAbout = styled.div``;
 
+export const ContainerColumn = styled.div`
+  width: 31.25rem;
+`;
+
+export const WrapperCard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  overflow: hidden;
+  justify-content: space-around;
+  margin-bottom: 32px;
+
+  @media (max-width: 735px) {
+    flex-direction: column;
+  }
+`;
+
 export const Image = styled.div`
   svg {
     color: #ffc107;
