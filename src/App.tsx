f5f699981c22813bbbe05e62138864ccf6cd03c5
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-06-18 16:12:53 -0300

    feat: test of the about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { HomePage } from "./pages/HomePage";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "./styles/ThemeProvider";
+import { About } from "./pages/About";
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <HomePage />
+      {/* <HomePage /> */}
+      <About />
     </ThemeProvider>
   );
 };
